0216068fd071c26448f32fc3d65c8865818c76ca
Zachariah Reitano <zreitano@gmail.com>
2016-07-15 15:43:51 -0400

    BUMMER

--- wwwroot/js/app.js
+++ wwwroot/js/app.js
@@ -3,12 +3,19 @@ var app = angular.module('imamuApp', ['ngRoute']);
  // configure our routes
 app.config(function($routeProvider, $locationProvider) {
 
-
+		$locationProvider.html5Mode({
+		  enabled: true,
+		  requireBase: false
+		});
         $routeProvider
 
         // route for the home page
         .when('/', {
             templateUrl : '/templates/home.html',
+            controller  : 'HomeController'
+        })
+        .when('/marketplace', {
+            templateUrl : '/templates/marketplace.html',
             controller  : 'HomeController'
         })
 
