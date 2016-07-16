c93b27baa254034396c1e7805c6ecdf940e6ea56
Zachariah Reitano <zreitano@gmail.com>
2016-07-16 11:23:43 -0400

    mixpanel working

--- wwwroot/js/controllers/HomeCtrl.js
+++ wwwroot/js/controllers/HomeCtrl.js
@@ -2,13 +2,27 @@
 
   var HomeController = function($scope) {
 
-    
+     $scope.createUser = createUser
+     $scope.trackEvent = trackEvent
+     $scope.path = window.location.pathname
 
      function init() {
 
      }
 
      init();
+
+     function createUser(user) {
+      var distinctId = mixpanel.get_distinct_id()
+      mixpanel.identify(distinctId)
+      mixpanel.people.set({
+          "$email": user.email
+      });
+     }
+
+     function trackEvent(action, info) {
+        mixpanel.track(action, info)
+     }
 
   };
 
